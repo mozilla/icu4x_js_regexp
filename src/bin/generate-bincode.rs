a9a89734057010cfd3d62941d6e9c87121526a03
Iain Ireland <iain.i.ireland@gmail.com>
2021-07-20 15:26:32 -0700

    Add tool to generate bincode from toml

---
+++ src/bin/generate-bincode.rs
@@ -0,0 +1,47 @@
+use std::error::Error;
+use std::path::PathBuf;
+
+use icu_provider::export::DataExporter;
+use icu_provider::ResourceKey;
+use icu_provider_blob::export::BlobExporter;
+use icu_provider_uprops::BinaryPropertiesDataProvider;
+
+fn raw_data_dir() -> PathBuf {
+    PathBuf::from(std::env!("CARGO_MANIFEST_DIR")).join("data/raw")
+}
+
+fn output_path() -> PathBuf {
+    PathBuf::from(std::env!("CARGO_MANIFEST_DIR")).join("data/uprops.bincode")
+}
+
+fn get_all_uprops_keys() -> Vec<ResourceKey> {
+    let mut keys = vec![];
+
+    // Keys are supported if a corresponding .toml file exists in `/data`.
+    for key in &icu_uniset::provider::key::ALL_KEYS {
+        let name = key.sub_category;
+        let mut path = raw_data_dir().clone().join(&*name);
+        path.set_extension("toml");
+        if path.exists() {
+            keys.push(*key);
+        }
+    }
+
+    keys
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let fs_provider = BinaryPropertiesDataProvider::new(raw_data_dir());
+
+    let sink = Box::new(std::fs::File::create(output_path())?);
+    let mut exporter = BlobExporter::new_with_sink(sink);
+
+    let keys = get_all_uprops_keys();
+    for key in keys.iter() {
+        icu_provider::export::export_from_iterable(key, &fs_provider, &mut exporter)?
+    }
+
+    exporter.close()?;
+
+    Ok(())
+}
